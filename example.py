fbaebc6529cef94cbd0b99b3a7fed5376434dff8
KingCosma <127743050+KingCosma@users.noreply.github.com>
2023-04-18 19:38:14 +0530

    Add files via upload

---
+++ example.py
@@ -0,0 +1,36 @@
+def htmlelementfy(shorthand):
+    #function to convert the given data into a html element
+    def elementfy(element, innervalue, attributes):
+        x = '<' + str(element)
+        
+        if attributes[0] == "":
+            has_attributes = False
+        else:
+            has_attributes = True
+            x = x + " "
+        for c in attributes:
+            x = x + str(c)
+            if has_attributes and c != attributes[len(attributes)-1]:
+                x = x + " "
+                
+        x = x + ">" + innervalue + "</" + element + ">"
+        return x
+    
+    #split the given string into variables to convert it into a html element
+    x = shorthand
+    y = x.split("(")
+    z = y[-1][:-1]
+    y[-1] = z
+    data = z.split(" #", 1)   
+    element = y[0]
+    innervalue = data[0]
+    attributes = data[1]
+    attributes = attributes.split(" #")
+
+    #return the html element
+    return elementfy(element, innervalue, attributes)
+
+#-------------------------------------------------------------------------------------------
+while True:
+    print("Type the shorthand")
+    print(htmlelementfy(input("")))
